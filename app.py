28d85c02f882bdd7d167b68dae3cfea9d5fc94b8
CodeNextPaco <paco@csedge.org>
2022-03-04 15:22:53 -0800

    removed files for starter

--- app.py
+++ app.py
@@ -1,39 +1,22 @@
 from flask import Flask, render_template, request
-import requests
 import sqlite3
 from datetime import datetime
 
 
 app = Flask(__name__)
 
+
+
+
 def get_all_fortunes():
-    print("getting all fortunes")
+
     #connect to DB
-    conn = sqlite3.connect('./static/data/fortuneteller.db')
-    curs = conn.cursor()
+ 
     all_fortunes = []
-    rows = curs.execute("SELECT * from fortunes")
-    for row in rows:
-        fortune = {'fortune': row[0],  'date': row[1]}
-        all_fortunes.append(fortune)
-    conn.close()
+
 
     return all_fortunes
 
-def get_all_users():
-    print("getting all users...")
-
-     #connect to DB
-    conn = sqlite3.connect('./static/data/fortuneteller.db')
-    curs = conn.cursor()
-    all_users = []
-    rows = curs.execute("SELECT * from users")
-    for row in rows:
-        user = {'name': row[0],  'username': row[1]}
-        all_users.append(user)
-    conn.close()
-
-    return all_users
 
 
 def store_fortune(new_fortune, fortune_date):
@@ -41,52 +24,38 @@ def store_fortune(new_fortune, fortune_date):
     print("Storing new Fortune!")
 
     #connect to database
-    conn = sqlite3.connect('./static/data/fortuneteller.db')
-    curs = conn.cursor()
+
     #insert a new row to the db and commit
-    curs.execute("INSERT INTO fortunes (fortune, date) VALUES((?),(?))",(new_fortune, fortune_date ))
-    conn.commit()
+    
     #close database connection
-    conn.close()
+  
 
 
 
 @app.route('/')
 def index():
+
+
     return render_template('index.html')
 
-@app.route('/login', methods=["POST"])
-def login():
-    print("Logging in...")
-
-    data = {
-        "message": "Welcome!",
-
-    }
-    return render_template('index.html', data=data)
+  
 
 @app.route('/admin')
 def admin_home():
 
-    fortunes = get_all_fortunes()
-    users = get_all_users()
+    #get all fortunes
+    fortunes = get_all_fortunes() 
+     
+    #call get_all_users
 
 
-    if len(fortunes) > 0:
-        print("We have fortunes to pass on!")
+    #pass on your fortunes and users to your client
+    data = {}
 
-        data = {
-            "all_fortunes": fortunes,
-            "all_users": users
-        }
-
-        return render_template('admin.html', data=data)
+    return render_template('admin.html', data=data)
 
        
 
-    return render_template('admin.html')
-        
-    
     
 
 
@@ -94,6 +63,7 @@ def admin_home():
 def post_fortune():
 
     print("posting fortune: ")
+    #get the fortune text from fortune-input
     fortune = request.form["fortune-input"]
     now = datetime.now() # current date and time   
 
@@ -103,16 +73,18 @@ def post_fortune():
     #Store the message in the DB
     store_fortune(fortune, date_time)
 
+    data ={}
     fortunes = get_all_fortunes()
+    #if there are fortunes, store them in the dictionary
     if len(fortunes) > 0:
         data = {
 
             "all_fortunes": fortunes
         }
 
-        return render_template('admin.html',data=data )
+        
 
-    return render_template('admin.html' )
+    return render_template('admin.html',data=data )
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
